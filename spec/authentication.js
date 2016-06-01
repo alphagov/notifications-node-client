b4291a119e6825c47cba8691f925db6d44426eeb
Chris Hill-Scott <me@quis.cc>
2016-06-01 14:50:08 +0100

    Remove `req` and `body` from JWT token
    We’ve eased up the restrictions on our JWT tokens so that they no longer have
    to be signed with the request method and path, and the body of the request (for
    `POST` requests).
    
    This happened here:
    https://github.com/alphagov/notifications-api/pull/226
    
    This commit makes similar changes to those we did for the Python client:
    https://github.com/alphagov/notifications-python-client/commit/277663e17b347514b59a1436bab64f0549203974

--- spec/authentication.js
+++ spec/authentication.js
@@ -16,29 +16,14 @@ describe('Authentication', function() {
 
   describe('tokens', function() {
 
-    it('should be generated without a payload', function() {
+    it('can be generated and decoded', function() {
 
       var token = create_govuk_notify_token("POST", "/notifications/sms", "SECRET", 123),
           decoded = jwt.verify(token, 'SECRET');
 
-      expect(token).to.equal('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjEyMywiaWF0IjoxMjM0NTY3ODkwLCJyZXEiOiJBOVlNVWcrSVk1eDZ0MFN1NUM0Z0t3V3J4YzZXYkZTYWV5RDZWRFUwcWc4PSJ9.v0nbBEimXMrpwH20kX4Xrv6M9I1wZiTQe_Dm8UjPpN0');
+      expect(token).to.equal('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjEyMywiaWF0IjoxMjM0NTY3ODkwfQ.R9-H_oV7d56Dal9jUAKThrZlo1_LNVqc3LCtY62WQd4');
       expect(decoded.iss).to.equal(123);
       expect(decoded.iat).to.equal(1234567890);
-      expect(decoded.req).to.equal('A9YMUg+IY5x6t0Su5C4gKwWrxc6WbFSaeyD6VDU0qg8=');
-      expect(decoded.pay).to.be.undefined;
-
-    });
-
-    it('should be generated with a payload', function() {
-
-      var token = create_govuk_notify_token("POST", "/notifications/sms", "SECRET", 456, "{'content': 'Hello world'}"),
-          decoded = jwt.verify(token, 'SECRET');
-
-      expect(token).to.equal('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjQ1NiwiaWF0IjoxMjM0NTY3ODkwLCJyZXEiOiJBOVlNVWcrSVk1eDZ0MFN1NUM0Z0t3V3J4YzZXYkZTYWV5RDZWRFUwcWc4PSIsInBheSI6IkhNcVhIUUtOVHVadUZHWVNGQzJ5T3I2N05VdXIxRmxja01UUHpHWVZVOFU9In0.rMiwF5HzFWdofyhIOJo08oEFRik1a4bqJn9I4K7M5kg');
-      expect(decoded.iss).to.equal(456);
-      expect(decoded.iat).to.equal(1234567890);
-      expect(decoded.req).to.equal('A9YMUg+IY5x6t0Su5C4gKwWrxc6WbFSaeyD6VDU0qg8=');
-      expect(decoded.pay).to.equal('HMqXHQKNTuZuFGYSFC2yOr67NUur1FlckMTPzGYVU8U=');
 
     });
 

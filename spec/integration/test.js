93da8ee7d3a175d9633247e56ebc392871a94f13
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2024-11-27 15:23:32 +0000

    Update integration tests to work in FT environment
    Also removes some assertions on the content of the templates. We already
    assert that the returned JSON has the correct schema, so these
    assertions served only to increase coupling between the tests and the
    fixtures, without improving coverage.
    
    Also bumps the number of retries when encountering a PDFNotReadyError,
    because the dev envs seem to take longer to process them than preview
    does.

--- spec/integration/test.js
+++ spec/integration/test.js
@@ -27,14 +27,14 @@ function make_random_id() {
 
 describer('notification api with a live service', function () {
   // default is 2000 (ms) - api is sometimes slower than this :(
-  this.timeout(40000)
+  this.timeout(120000)
 
   let notifyClient;
   let teamNotifyClient;
   let emailNotificationId;
   let smsNotificationId;
   let letterNotificationId;
-  const personalisation = { name: 'Foo' };
+  const personalisation = { build_id: 'Foo' };
   const clientRef = 'client-ref';
   const oneClickUnsubscribeURL = 'https://www.example.com';
   const email = process.env.FUNCTIONAL_TEST_EMAIL;
@@ -43,6 +43,7 @@ describer('notification api with a live service', function () {
     address_line_1: make_random_id(),
     address_line_2: 'Foo',
     postcode: 'SW1 1AA',
+    build_id: 'Foo',
   };
   const smsTemplateId = process.env.FUNCTIONAL_TEST_SMS_TEMPLATE_ID;
   const smsSenderId = process.env.FUNCTIONAL_TESTS_SERVICE_SMS_SENDER_ID || undefined;
@@ -72,8 +73,7 @@ describer('notification api with a live service', function () {
       return notifyClient.sendEmail(emailTemplateId, email, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postEmailNotificationResponseJson);
-        response.data.content.body.should.equal('Hello Foo\r\n\r\nFunctional test help make our world a better place');
-        response.data.content.subject.should.equal('Functional Tests are good');
+        response.data.content.body.should.contain('Foo');
         response.data.reference.should.equal(clientRef);
         emailNotificationId = response.data.id;
       })
@@ -87,8 +87,7 @@ describer('notification api with a live service', function () {
       return notifyClient.sendEmail(emailTemplateId, email, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postEmailNotificationResponseJson);
-        response.data.content.body.should.equal('Hello Foo\r\n\r\nFunctional test help make our world a better place');
-        response.data.content.subject.should.equal('Functional Tests are good');
+        response.data.content.body.should.contain('Foo');
         should.equal(response.data.content.one_click_unsubscribe_url, null);
         response.data.reference.should.equal(clientRef);
         emailNotificationId = response.data.id;
@@ -102,8 +101,7 @@ describer('notification api with a live service', function () {
       return notifyClient.sendEmail(emailTemplateId, email, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postEmailNotificationResponseJson);
-        response.data.content.body.should.equal('Hello Foo\r\n\r\nFunctional test help make our world a better place');
-        response.data.content.subject.should.equal('Functional Tests are good');
+        response.data.content.body.should.contain('Foo');
         response.data.content.one_click_unsubscribe_url.should.equal(oneClickUnsubscribeURL);
         response.data.reference.should.equal(clientRef);
         emailNotificationId = response.data.id;
@@ -112,15 +110,14 @@ describer('notification api with a live service', function () {
 
     it('send email notification with document upload', () => {
       var postEmailNotificationResponseJson = require('./schemas/v2/POST_notification_email_response.json'),
-        options = {personalisation: { name: 'Foo', documents:
+        options = {personalisation: { build_id: 'Foo', documents:
           notifyClient.prepareUpload(Buffer.from("%PDF-1.5 testpdf"))
         }, reference: clientRef};
 
       return notifyClient.sendEmail(emailTemplateId, email, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postEmailNotificationResponseJson);
-        response.data.content.body.should.equal('Hello Foo\r\n\r\nFunctional test help make our world a better place');
-        response.data.content.subject.should.equal('Functional Tests are good');
+        response.data.content.body.should.contain('Foo');
         response.data.reference.should.equal(clientRef);
         emailNotificationId = response.data.id;
       })
@@ -128,15 +125,14 @@ describer('notification api with a live service', function () {
 
     it('send email notification with CSV document upload', () => {
       var postEmailNotificationResponseJson = require('./schemas/v2/POST_notification_email_response.json'),
-        options = {personalisation: { name: 'Foo', documents:
+        options = {personalisation: { build_id: 'Foo', documents:
           notifyClient.prepareUpload(Buffer.from("a,b"), true)
         }, reference: clientRef};
 
       return notifyClient.sendEmail(emailTemplateId, email, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postEmailNotificationResponseJson);
-        response.data.content.body.should.equal('Hello Foo\r\n\r\nFunctional test help make our world a better place');
-        response.data.content.subject.should.equal('Functional Tests are good');
+        response.data.content.body.should.contain('Foo');
         response.data.reference.should.equal(clientRef);
         emailNotificationId = response.data.id;
       })
@@ -149,7 +145,7 @@ describer('notification api with a live service', function () {
       return notifyClient.sendSms(smsTemplateId, phoneNumber, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postSmsNotificationResponseJson);
-        response.data.content.body.should.equal('Hello Foo\n\nFunctional Tests make our world a better place');
+        response.data.content.body.should.contain('Foo');
         smsNotificationId = response.data.id;
       });
     });
@@ -162,7 +158,7 @@ describer('notification api with a live service', function () {
       return teamNotifyClient.sendSms(smsTemplateId, phoneNumber, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postSmsNotificationResponseJson);
-        response.data.content.body.should.equal('Hello Foo\n\nFunctional Tests make our world a better place');
+        response.data.content.body.should.contain('Foo')
         smsNotificationId = response.data.id;
       });
     });
@@ -174,7 +170,7 @@ describer('notification api with a live service', function () {
       return notifyClient.sendLetter(letterTemplateId, options).then((response) => {
         response.status.should.equal(201);
         expect(response.data).to.be.jsonSchema(postLetterNotificationResponseJson);
-        response.data.content.body.should.equal('Hello ' + letterContact.address_line_1);
+        response.data.content.body.should.contain('Foo');
         letterNotificationId = response.data.id;
       });
     });
@@ -202,8 +198,7 @@ describer('notification api with a live service', function () {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getNotificationJson);
         response.data.type.should.equal('email');
-        response.data.body.should.equal('Hello Foo\r\n\r\nFunctional test help make our world a better place');
-        response.data.subject.should.equal('Functional Tests are good');
+        response.data.body.should.contain('Foo')
       });
     });
 
@@ -213,7 +208,7 @@ describer('notification api with a live service', function () {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getNotificationJson);
         response.data.type.should.equal('sms');
-        response.data.body.should.equal('Hello Foo\n\nFunctional Tests make our world a better place');
+        response.data.body.should.contain('Foo')
       });
     });
 
@@ -223,7 +218,7 @@ describer('notification api with a live service', function () {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getNotificationJson);
         response.data.type.should.equal('letter');
-        response.data.body.should.equal('Hello ' + letterContact.address_line_1);
+        response.data.body.should.contain('Foo')
       });
     });
 
@@ -247,7 +242,7 @@ describer('notification api with a live service', function () {
           }
           if (err.response.data && (err.response.data.errors[0].error === "PDFNotReadyError")) {
             count += 1
-            if (count < 7) {
+            if (count < 24) {
               setTimeout(tryClient, 5000)
             } else {
               done(new Error('Too many PDFNotReadyError errors'));
@@ -281,7 +276,6 @@ describer('notification api with a live service', function () {
       return notifyClient.getTemplateById(smsTemplateId).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getTemplateJson);
-        response.data.name.should.equal('Client Functional test sms template');
         should.not.exist(response.data.subject);
         should.not.exist(response.data.letter_contact_block);
       });
@@ -291,9 +285,6 @@ describer('notification api with a live service', function () {
       return notifyClient.getTemplateById(emailTemplateId).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getTemplateJson);
-        response.data.body.should.equal('Hello ((name))\r\n\r\nFunctional test help make our world a better place');
-        response.data.name.should.equal('Client Functional test email template');
-        response.data.subject.should.equal('Functional Tests are good');
         should.not.exist(response.data.letter_contact_block);
       });
     });
@@ -302,13 +293,7 @@ describer('notification api with a live service', function () {
       return notifyClient.getTemplateById(letterTemplateId).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getTemplateJson);
-        response.data.body.should.equal('Hello ((address_line_1))');
-        response.data.name.should.equal('Client functional letter template');
-        response.data.subject.should.equal('Main heading');
-        response.data.letter_contact_block.should.equal(
-          'Government Digital Service\nThe White Chapel Building\n' +
-          '10 Whitechapel High Street\nLondon\nE1 8QS\nUnited Kingdom'
-        );
+        should.exist(response.data.letter_contact_block)
       });
     });
 
@@ -316,7 +301,6 @@ describer('notification api with a live service', function () {
       return notifyClient.getTemplateByIdAndVersion(smsTemplateId, 1).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getTemplateJson);
-        response.data.name.should.equal('Example text message template');
         should.not.exist(response.data.subject);
         response.data.version.should.equal(1);
         should.not.exist(response.data.letter_contact_block);
@@ -327,19 +311,7 @@ describer('notification api with a live service', function () {
       return notifyClient.getTemplateByIdAndVersion(emailTemplateId, 1).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(getTemplateJson);
-        response.data.name.should.equal('Client Functional test email template');
         response.data.version.should.equal(1);
-      });
-    });
-
-    it('get letter template by id and version', () => {
-      return notifyClient.getTemplateByIdAndVersion(letterTemplateId, 1).then((response) => {
-        response.status.should.equal(200);
-        expect(response.data).to.be.jsonSchema(getTemplateJson);
-        response.data.name.should.equal('Untitled');
-        response.data.version.should.equal(1);
-        // version 1 of the template had no letter_contact_block
-        should.not.exist(response.data.letter_contact_block);
       });
     });
 
@@ -372,7 +344,7 @@ describer('notification api with a live service', function () {
     });
 
     it('preview sms template', () => {
-      var personalisation = { "name": "Foo" }
+      var personalisation = { "build_id": "Foo" }
       return notifyClient.previewTemplateById(smsTemplateId, personalisation).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(postTemplatePreviewJson);
@@ -382,7 +354,7 @@ describer('notification api with a live service', function () {
     });
 
     it('preview email template', () => {
-      var personalisation = { "name": "Foo" }
+      var personalisation = { "build_id": "Foo" }
       return notifyClient.previewTemplateById(emailTemplateId, personalisation).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(postTemplatePreviewJson);
@@ -392,7 +364,7 @@ describer('notification api with a live service', function () {
     });
 
     it('preview letter template', () => {
-      var personalisation = { "address_line_1": "Foo", "address_line_2": "Bar", "postcode": "SW1 1AA" }
+      var personalisation = { "address_line_1": "Foo", "address_line_2": "Bar", "postcode": "SW1 1AA", "build_id": "Foo" }
       return notifyClient.previewTemplateById(letterTemplateId, personalisation).then((response) => {
         response.status.should.equal(200);
         expect(response.data).to.be.jsonSchema(postTemplatePreviewJson);
